4325cbc82dae15b0f8bbb8d2854cc07cfdac82fc
Mike Lawrence <mike@axemneuro.com>
2022-06-14 04:54:32 -0300

    sdl2 gamepad and update gitignore

--- app_code/gamepad.py
+++ app_code/gamepad.py
@@ -10,140 +10,60 @@ debug.check_vars(['rx_dict', 'tx_dict'])
 
 #library imports
 
-import usb
+import sdl2 #for input and display
+import sdl2.ext #for input and display
 import sys
 import time
 import copy
 import appnope
 appnope.nope()
 
-#find, claim & configure the gamepad
-dev = usb.core.find(idVendor=1118, idProduct=654) #wired 360 gamepad
-if dev.is_kernel_driver_active(0):
-	dev.detach_kernel_driver(0)
-dev.set_configuration()
-usb.util.claim_interface(dev, 0)
-read_endpoint = dev[0][(0,0)][0] #endpoint to read from
-write_endpoint = dev[0][(0,0)][1] #endpoint to write to
+sdl2.SDL_Init(sdl2.SDL_INIT_JOYSTICK)
+sdl2.SDL_SetHint(sdl2.hints.SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS,b"1")
 
-#turn the LED off
-dev.write(write_endpoint,"\x01\x03\x00",0)
-last_data = None
+joystick = sdl2.SDL_JoystickOpen(0)
+name = sdl2.joystick.SDL_JoystickName(joystick).decode("UTF-8")
 
-button_names = {\
-	'2': {\
-		'1':'dpad-up'\
-		, '2':'dpad-down'\
-		, '4':'dpad-left'\
-		, '8':'dpad-right'\
-		, '16':'start'\
-		, '32':'back'\
-		, '64':'left-stick'\
-		, '128':'right-stick'\
-	}\
-	, '3': {\
-		'1':'LB'\
-		, '2':'RB'\
-		, '4':'xbox'\
-		, '8':''\
-		, '16':'A'\
-		, '32':'B'\
-		, '64':'X'\
-		, '128':'Y'\
-	}\
+col_info = {
+	'trigger':{'col':1,'mapping':{ 'left':2, 'right':5},
+	'time':{'col':2},
+	'value':{'col':3},
 }
+tx_dict['writer'].put(kind='attr',payload={'name':'col_info','value':json.dumps(col_info)})
 
-last_buttons_down = {\
-	'2': {\
-		'1':False\
-		, '2':False\
-		, '4':False\
-		, '8':False\
-		, '16':False\
-		, '32':False\
-		, '64':False\
-		, '128':False\
-	}\
-	, '3': {\
-		'1':False\
-		, '2':False\
-		, '4':False\
-		, '8':False\
-		, '16':False\
-		, '32':False\
-		, '64':False\
-		, '128':False\
-	}\
-}
-
-#define a useful function for processing button input
-def process_buttons(button_set,now,data,last_buttons_down):
-	buttons_down = copy.deepcopy(last_buttons_down)
-	state = data[button_set]
-	events = []
-	for i in [128,64,32,16,8,4,2,1]:
-		if state>=i:
-			buttons_down[str(button_set)][str(i)] = True
-			state = state - i
-		else:
-			buttons_down[str(button_set)][str(i)] = False
-		# for i in [128,64,32,16,8,4,2,1]:
-		#print [i,buttons_down[str(button_set)][str(i)],last_buttons_down[str(button_set)][str(i)]]
-		if buttons_down[str(button_set)][str(i)]!=last_buttons_down[str(button_set)][str(i)]:
-			message = {}
-			message['time'] = now
-			if buttons_down[str(button_set)][str(i)]:
-				message['type'] = 'buttonDown'
-			else:
-				message['type'] = 'buttonUp'
-			message['name'] = button_names[str(button_set)][str(i)]
-			events.append(message)
-	return [events,buttons_down]
-
-def send_event(message):
-	tx_dict['exp'].put(kind='event',payload=message)
-	tx_dict['writer'].put(kind='event',payload=message)
-
+triggers = dict()
+for side,index in {'left':2,'right':5}.items():
+	triggers[side] = dict()
+	triggers[side]['name'] = side
+	triggers[side]['index'] = index
+	triggers[side]['last_value'] = -(2**16)/2
+	triggers[side]['last_time'] = time.perf_counter()
+	triggers[side]['response_sent'] = False
 
 while True:
 	#check if there are any messages from the parent process
 	if not rx_dict['parent'].empty():
 		message = rx_dict['parent'].get()
 		if message.kind == 'quit':
-			usb.util.release_interface(dev, 0)
 			sys.exit()
 	#get the current time
 	now = time.perf_counter()
 	#check if there's any data from the gamepad
-	try:
-		data = dev.read(read_endpoint.bEndpointAddress,read_endpoint.wMaxPacketSize,0)
-	except usb.core.USBError as e:
-		data = None
-		if e.args == ('Operation timed out',):
-			continue
-	#process the data from the gamepad
-	if data is not None:
-		if len(data)==20: #getting some button/axis state data
-			if last_data is not None:
-				for button_set in [2,3]: #check both button sets
-					if last_data[button_set]!=data[button_set]: #check buttons associated with state 2
-						button_events,last_buttons_down = process_buttons(button_set=button_set,now=now,data=data,last_buttons_down=last_buttons_down)
-						for this_button_event in button_events:
-							message = {}
-							message['type'] = 'button'
-							message['id'] = this_button_event
-							message['time'] = now
-							send_event(message)
-				for trigger in [4,5]:
-					if last_data[trigger]!=data[trigger]:
-						message = {}
-						message['type'] = 'trigger'
-						if trigger==4:
-							message['id'] = 'left'
-						else:
-							message['id'] = 'right'
-						message['time'] = now
-						message['value'] = data[trigger]
-						send_event(message)
-			#write/overwrite last_data with current data
-			last_data = data
+	sdl2.SDL_PumpEvents()
+	for trigger in triggers.items():
+		new_value = sdl2.SDL_JoystickGetAxis(joystick,trigger['index'])
+		if new_value!=trigger['last_value']:
+			trigger['last_time'] = time.perf_counter()
+			trigger['last_value'] = new_value
+			tx_dict['writer'].put(kind='data',payload=)
+			to_write = np.array([[trigger['index'],trigger['last_time'],trigger['last_value']]],dtype=np.float64)
+			tx_dict['writer'].put(kind='data',payload=copy.deepcopy(to_write))
+			if new_value>trigger['last_value']:
+				if new_value>0:
+					if not trigger['response_sent']:
+						tx_dict['exp'].put(kind=trigger['name'],payload=trigger['last_time'])
+						trigger['response_sent'] = True
+			else:
+				if new_value<0:
+					if trigger['response_sent']:
+						trigger['response_sent'] = False
